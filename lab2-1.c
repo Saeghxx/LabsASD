0ffe6875bd9baf350a603b017b3120a89b188715
Saeghxx <alina.retiznik2007@gmail.com>
2025-11-02 21:09:38 +0200

    ASD

--- lab2-1.c
+++ lab2-1.c
@@ -14,21 +14,26 @@ point_zero:
     {
         for (int i = 1; i <= n; i++)
         {
-            double resj = 1.0; 
+            double resj = 1.0;
             for (int j = 1; j <= i; j++) {
                 resj *= (j + 1);
                 OpCount++;
             }
 
-            double denom = pow(2.0, i) + 1; 
-            OpCount += 2;
+            double denom = 1.0;
+            for (int k = 1; k <= i; k++) {
+                denom *= 2.0;
+                OpCount++;
+            }
+            denom += 1.0;
+            OpCount++;
 
-            res += resj / (denom * denom); 
+            res += resj / (denom * denom);
             OpCount += 3;
         }
         printf("result = %.7lf \nOperations %d \n", res, OpCount);
-    }
-    else { 
+    } 
+    else {
         goto point_zero;
     }
 
--- lab2-2.c
+++ lab2-2.c
@@ -5,38 +5,37 @@ int main()
 {
     int n;
     int OpCount = 0;
-    double res = 0.0;
-    double ressum = 0.0;
+    double ressum = 0;
 
 point_zero:
     printf("n = ");
     scanf("%d", &n);
-    
+
     if (n >= 1)
     {
+        double resj = 1.0;  
+        double pow2 = 1.0;     
         for (int i = 1; i <= n; i++)
         {
-            double resj = 1.0; 
-            for (int j = 1; j <= i; j++) {
-                resj *= (j + 1);
-                OpCount++;
-            }
-
-            double denom = pow(2.0, i) + 1; 
-            OpCount += 2;
-
-            ressum = resj / (denom * denom);
-            OpCount += 3;
-
-            res += ressum; 
+            resj *= (i + 1);
             OpCount++;
+
+            pow2 *= 2.0; 
+            OpCount++;
+
+            double denom = pow2 + 1.0;
+            OpCount++; 
+
+            ressum += resj / (denom * denom);
+            OpCount += 3;
         }
-        printf("Res = %.7lf \nOperation = %d\n", res, OpCount);
+
+        printf("S = %.7lf\nOperations = %d\n", ressum, OpCount);
     }
     else
     {
         goto point_zero;
     }
-    
+
     return 0;
 }
