26ace99c5c70e2e98c7adf00022b5ecaadd93b7a
Saeghxx <alina.retiznik2007@gmail.com>
2025-11-23 00:11:10 +0200

    gg

--- lab3.c
+++ lab3.c
@@ -19,54 +19,59 @@ int main(int argc, char *argv[])
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols; j++)
-        {
             printf("~");
-        }
         printf("\n");
     }
 
-    pos.X = cols / 2;
-    pos.Y = rows / 2;
+    pos.X = cols - rows / 2;
+    pos.Y = rows / 2 - 1;
 
-    int step = 1;     
-    int moves = 0;        
-    int changes = 0;      
-    int direction = 0;    
+    int colCoord = cols - rows + 1;
+    int rowCoord = 1;
 
-    while (1)
+    int direction = 0; 
+
+    while (colCoord < 81 && rowCoord < 25)
     {
-    
-        SetConsoleCursorPosition(hout, pos);
-        printf("#");
-        fflush(stdout);
-        usleep(1000);
-
-        switch (direction)
+       
+        if (colCoord < 81)
         {
-        case 0: pos.X++; break; 
-        case 1: pos.Y++; break; 
-        case 2: pos.X--; break; 
-        case 3: pos.Y--; break; 
+            for (int i = 0; i < colCoord; i++)
+            {
+                SetConsoleCursorPosition(hout, pos);
+                printf("*");
+                fflush(stdout);
+                usleep(1000);
+
+                if (direction == 0)
+                    pos.X--; 
+                else
+                    pos.X++; 
+            }
+            colCoord++;
+        }
+        if (rowCoord < 24)
+        {
+            for (int i = 0; i < rowCoord; i++)
+            {
+                SetConsoleCursorPosition(hout, pos);
+                printf("*");
+                fflush(stdout);
+                usleep(1000);
+
+                if (direction == 0)
+                    pos.Y--; 
+                else
+                    pos.Y++; 
+            }
+            rowCoord++;
         }
 
-        moves++;
-
-        if (moves == step)
-        {
-            moves = 0;
-            direction = (direction + 1) % 4;
-            changes++;
-
-            if (changes % 2 == 0)
-                step++;
-        }
-
-        if (pos.X < 0 || pos.X >= cols || pos.Y < 0 || pos.Y >= rows)
-            break;
+        direction = !direction;
     }
 
     COORD endPos = {0, rows};
     SetConsoleCursorPosition(hout, endPos);
     getchar();
     return 0;
-}
+} 
---
+++ lab33333.c
@@ -0,0 +1,74 @@
+#include <stdio.h> 
+#include <windows.h> 
+#include <unistd.h> 
+ 
+int main(int argc, char *argv[]) 
+{ 
+    if (argc < 2 || (argv[1] == "--terminal") != 0) 
+    { 
+        system("start lab3.exe --terminal"); 
+        return 0; 
+    } 
+ 
+    HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE); 
+    COORD pos; 
+ 
+    int rows = 24; 
+    int cols = 80; 
+ 
+    for (int i = 0; i < rows; i++) 
+    { 
+        for (int j = 0; j < cols; j++) 
+        { 
+            printf("~"); 
+        } 
+        printf("\n"); 
+    } 
+ 
+ 
+    pos.X = cols / 2; 
+    pos.Y = rows / 2; 
+ 
+    int step = 1;      
+    int moves = 0;         
+    int changes = 0;       
+    int direction = 0;     
+ 
+    while (1) 
+    { 
+     
+        SetConsoleCursorPosition(hout, pos); 
+        printf("#"); 
+        fflush(stdout); 
+        usleep(1000); 
+ 
+        switch (direction) 
+        { 
+        case 0: pos.Y--; break;  
+        case 1: pos.X--; break;  
+        case 2: pos.X++; break;  
+        case 3: pos.Y++; break;  
+        } 
+ 
+        moves++; 
+ 
+        if (moves == step) 
+        { 
+ 
+            moves = 0; 
+            direction = (direction + 1) % 4; 
+            changes++; 
+ 
+            if (changes % 2 == 0) 
+                step++; 
+        } 
+ 
+        if (pos.X < 0 || pos.X >= cols || pos.Y < 0 || pos.Y >= rows) 
+            break; 
+    } 
+ 
+    COORD endPos = {0, rows}; 
+    SetConsoleCursorPosition(hout, endPos); 
+    getchar(); 
+    return 0; 
+}  
